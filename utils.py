aad65f0b35bce1f96529c72762fa417ff567a3c5
Chloe Zhang <46570430+Messier42@users.noreply.github.com>
2019-08-06 19:08:07 +0800

    Update utils.py
    Delete some unuseful function.

--- utils.py
+++ utils.py
@@ -1,52 +1,28 @@
 import numpy as np
 import torch
 import torchfile
-#from torch.utils.serialization import  torchfile
 import os
 import scipy.io as sio
 import cv2
 import math
 from math import cos, sin
+import base64
 
-def softmax_temperature(tensor, temperature):
-    result = torch.exp(tensor / temperature)
-    result = torch.div(result, torch.sum(result, 1).unsqueeze(1).expand_as(result))
-    return result
+from PIL import Image
+from io import BytesIO 
 
-def get_pose_params_from_mat(mat_path):
-    # This functions gets the pose parameters from the .mat
-    # Annotations that come with the Pose_300W_LP dataset.
-    mat = sio.loadmat(mat_path)
-    # [pitch yaw roll tdx tdy tdz scale_factor]
-    pre_pose_params = mat['Pose_Para'][0]
-    # Get [pitch, yaw, roll, tdx, tdy]
-    pose_params = pre_pose_params[:5]
-    return pose_params
+#视频转码
+def frame2base64(frame):   
+    #print('detecting face') 
+    img = Image.fromarray(frame) #将每一帧转为Image    
+    output_buffer = BytesIO() #创建一个BytesIO    
+    img.save(output_buffer, format='JPEG') #写入output_buffer    
+    byte_data = output_buffer.getvalue() #在内存中读取    
+    base64_data = base64.b64encode(byte_data) #转为BASE64    
+    return base64_data #转码成功 返回base64编码 
 
-def get_ypr_from_mat(mat_path):
-    # Get yaw, pitch, roll from .mat annotation.
-    # They are in radians
-    mat = sio.loadmat(mat_path)
-    # [pitch yaw roll tdx tdy tdz scale_factor]
-    pre_pose_params = mat['Pose_Para'][0]
-    # Get [pitch, yaw, roll]
-    pose_params = pre_pose_params[:3]
-    return pose_params
-
-def get_pt2d_from_mat(mat_path):
-    # Get 2D landmarks
-    mat = sio.loadmat(mat_path)
-    pt2d = mat['pt2d']
-    return pt2d
-
-def mse_loss(input, target):
-    return torch.sum(torch.abs(input.data - target.data) ** 2)
-
-def plot_pose_cube(img, yaw, pitch, roll, tdx=None, tdy=None, size=150.):
-    # Input is a cv2 image
-    # pose_params: (pitch, yaw, roll, tdx, tdy)
-    # Where (tdx, tdy) is the translation of the face.
-    # For pose we have [pitch yaw roll tdx tdy tdz scale_factor]
+#画人脸框
+def plot_pose_base(img, yaw, pitch, roll, tdx=None, tdy=None, size=300):
 
     p = pitch * np.pi / 180
     y = -(yaw * np.pi / 180)
@@ -66,21 +42,11 @@ def plot_pose_cube(img, yaw, pitch, roll, tdx=None, tdy=None, size=150.):
     x3 = size * (sin(y)) + face_x
     y3 = size * (-cos(y) * sin(p)) + face_y
 
-    # Draw base in red
-    cv2.line(img, (int(face_x), int(face_y)), (int(x1),int(y1)),(0,0,255),3)
-    cv2.line(img, (int(face_x), int(face_y)), (int(x2),int(y2)),(0,0,255),3)
-    cv2.line(img, (int(x2), int(y2)), (int(x2+x1-face_x),int(y2+y1-face_y)),(0,0,255),3)
-    cv2.line(img, (int(x1), int(y1)), (int(x1+x2-face_x),int(y1+y2-face_y)),(0,0,255),3)
-    # Draw pillars in blue
-    cv2.line(img, (int(face_x), int(face_y)), (int(x3),int(y3)),(255,0,0),2)
-    cv2.line(img, (int(x1), int(y1)), (int(x1+x3-face_x),int(y1+y3-face_y)),(255,0,0),2)
-    cv2.line(img, (int(x2), int(y2)), (int(x2+x3-face_x),int(y2+y3-face_y)),(255,0,0),2)
-    cv2.line(img, (int(x2+x1-face_x),int(y2+y1-face_y)), (int(x3+x1+x2-2*face_x),int(y3+y2+y1-2*face_y)),(255,0,0),2)
-    # Draw top in green
-    cv2.line(img, (int(x3+x1-face_x),int(y3+y1-face_y)), (int(x3+x1+x2-2*face_x),int(y3+y2+y1-2*face_y)),(0,255,0),2)
-    cv2.line(img, (int(x2+x3-face_x),int(y2+y3-face_y)), (int(x3+x1+x2-2*face_x),int(y3+y2+y1-2*face_y)),(0,255,0),2)
-    cv2.line(img, (int(x3), int(y3)), (int(x3+x1-face_x),int(y3+y1-face_y)),(0,255,0),2)
-    cv2.line(img, (int(x3), int(y3)), (int(x3+x2-face_x),int(y3+y2-face_y)),(0,255,0),2)
+    # Draw base in white
+    cv2.line(img, (int(face_x), int(face_y)), (int(x1),int(y1)),(255,255,255),3)
+    cv2.line(img, (int(face_x), int(face_y)), (int(x2),int(y2)),(255,255,255),3)
+    cv2.line(img, (int(x2), int(y2)), (int(x2+x1-face_x),int(y2+y1-face_y)),(255,255,255),3)
+    cv2.line(img, (int(x1), int(y1)), (int(x1+x2-face_x),int(y1+y2-face_y)),(255,255,255),3)
 
     return img
 
